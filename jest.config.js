2b8ca388ab992a4918653a5576eebb8fbea079ad
Maikel Bos <maikel.bos0@gmail.com>
2023-08-19 19:59:53 +0200

    Clean up jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-    transform: { '^.+\\.ts?$': 'ts-jest' },
-    testEnvironment: 'node',
-    testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+    preset: "ts-jest"
 };
