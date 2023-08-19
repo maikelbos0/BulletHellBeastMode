db9c69c83f8004aae6ed6be1f66d7f6363f1b9f3
Maikel Bos <maikel.bos0@gmail.com>
2023-08-19 16:32:26 +0200

    Create an actual vector class

--- experiments/js/vector.js
+++ experiments/js/vector.js
@@ -1,3 +1,11 @@
-var message = 'Hello World';
-console.log(message);
+var Vector = /** @class */ (function () {
+    function Vector(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    Vector.prototype.getSpeed = function () {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    };
+    return Vector;
+}());
 //# sourceMappingURL=vector.js.map
--- experiments/vector.ts
+++ experiments/vector.ts
@@ -1,2 +1,13 @@
-let message: string = 'Hello World';
-console.log(message);
+class Vector {
+    x: number;
+    y: number;
+   
+    constructor(x: number, y: number) {
+      this.x = x;
+      this.y = y;
+    }
+   
+    getSpeed() : number {
+      return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+  }
