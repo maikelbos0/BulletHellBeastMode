db33cd3e12886e9da41d95ef396bc73a609c2345
Maikel Bos <maikel.bos0@gmail.com>
2023-08-21 09:59:04 +0200

    Improve existing test setup

--- tests/vector.test.ts
+++ tests/vector.test.ts
@@ -2,28 +2,24 @@ import { Vector } from '../src/vector';
 
 describe('Vector', () => {
     it.each([
-        [3, 4, 5],
-        [3, -4, 5],
-        [-3, 4, 5],
-        [-3, -4, 5]
-    ])('getMagnitude() x: %p, y: %p, expectedMagnitude: %p', (x: number, y: number, expectedMagnitude: number) => {
-        const subject = new Vector(x, y);
-
+        [new Vector(3, 4), 5],
+        [new Vector(3, -4), 5],
+        [new Vector(-3, 4), 5],
+        [new Vector(-3, -4), 5]
+    ])('getMagnitude() subject: %p, expectedMagnitude: %p', (subject: Vector, expectedMagnitude: number) => {
         expect(subject.getMagnitude()).toBe(expectedMagnitude);
     });
 
     it.each([
-        [6, 8, 5, 3, 4],
-        [-6, -8, 5, -3, -4],
-        [6, 8, 7.5, 4.5, 6],
-        [3, 4, 5, 3, 4],
-        [3, 4, 7.5, 3, 4]
-    ])('limitMagnitude() x: %p, y: %p, maximumMagnitude: %p, expectedX: %p, expectedY: %p', (x: number, y: number, maximumMagnitude: number, expectedX: number, expectedY: number) => {
-        const subject = new Vector(x, y);
-
+        [new Vector(6, 8), 5, new Vector(3, 4)],
+        [new Vector(-6, -8), 5, new Vector(-3, -4)],
+        [new Vector(6, 8), 7.5, new Vector(4.5, 6)],
+        [new Vector(3, 4), 5, new Vector(3, 4)],
+        [new Vector(3, 4), 7.5, new Vector(3, 4)]
+    ])('limitMagnitude() subject: %p, maximumMagnitude: %p, expectedResult: %p', (subject: Vector, maximumMagnitude: number, expectedResult: Vector) => {
         const result = subject.limitMagnitude(maximumMagnitude);
 
-        expect(result.x).toBe(expectedX);
-        expect(result.y).toBe(expectedY);
+        expect(result.x).toBe(expectedResult.x);
+        expect(result.y).toBe(expectedResult.y);
     });
 });
