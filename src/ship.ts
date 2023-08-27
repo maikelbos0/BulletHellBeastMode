66c6410c2e051ae07026a62d9a3ac15d7791554a
Maikel Bos <maikel.bos0@gmail.com>
2023-08-27 10:07:56 +0200

    Disable custom acceleration and limits while rewriting

--- src/ship.ts
+++ src/ship.ts
@@ -62,24 +62,25 @@ export class Ship {
 
     // TODO calculate desired speed from delta using max acceleration as deceleration when approaching target, with max speed? Use that in processframe?
     processFrame(direction: Direction, customAcceleration: Acceleration, duration: number) {
-        let isStopped = this.velocity.x == 0 && this.velocity.y == 0;
-        let isAccelerating = direction != Direction.None || customAcceleration.x != 0 || customAcceleration.y != 0;
-        let isDecelerating = !isStopped && !isAccelerating;
+        // let isStopped = this.velocity.x == 0 && this.velocity.y == 0;
+        // let isAccelerating = direction != Direction.None || customAcceleration.x != 0 || customAcceleration.y != 0;
+        // let isDecelerating = !isStopped && !isAccelerating;
         let acceleration = this.getDirectionalAcceleration(direction, duration);
 
-        if (isAccelerating) {
-            acceleration = acceleration.add(customAcceleration).limitMagnitude(Ship.maximumAcceleration);
-        }
-        else if (isDecelerating) {
-            acceleration = new Acceleration(-this.velocity.x, -this.velocity.y).adjustMagnitude(Ship.maximumAcceleration);
+        // if (isAccelerating) {
+        //     acceleration = acceleration.add(customAcceleration).limitMagnitude(Ship.maximumAcceleration);
+        // }
+        // else if (isDecelerating) {
+        //     acceleration = new Acceleration(-this.velocity.x, -this.velocity.y).adjustMagnitude(Ship.maximumAcceleration);
 
-            duration = Math.min(duration, this.velocity.getMagnitude() / acceleration.getMagnitude());
-        }
+        //     duration = Math.min(duration, this.velocity.getMagnitude() / acceleration.getMagnitude());
+        // }
 
-        if (isAccelerating || isDecelerating) {
-            this.position = this.position.move(this.velocity, duration / 2);
-            this.velocity = this.velocity.accelerate(acceleration, duration).limitMagnitude(Ship.maximumSpeed);
-            this.position = this.position.move(this.velocity, duration / 2);
-        }
+        // if (isAccelerating || isDecelerating) {
+            this.position = this.position.move(this.directionalVelocity, duration / 2);
+            // this.velocity = this.velocity.accelerate(acceleration, duration).limitMagnitude(Ship.maximumSpeed);
+            this.directionalVelocity = this.directionalVelocity.accelerate(acceleration, duration).limitMagnitude(Ship.maximumSpeed);
+            this.position = this.position.move(this.directionalVelocity, duration / 2);
+        // }
     }
 }
--- tests/ship.test.ts
+++ tests/ship.test.ts
@@ -47,37 +47,39 @@ describe('Ship', () => {
 
     it.each([
         // Directional acceleration
-        [new Coordinates(100, 100), new Velocity(50, 50), Direction.Down, new Acceleration(0, 0), 1, new Velocity(50, 250), new Coordinates(150, 250)],
-        [new Coordinates(100, 100), new Velocity(50, 50), Direction.Down, new Acceleration(0, 0), 2, new Velocity(50, 450), new Coordinates(200, 600)],
-        [new Coordinates(100, 100), new Velocity(400, 300), Direction.None, new Acceleration(0, 0), 1, new Velocity(160, 120), new Coordinates(380, 310)],
-        [new Coordinates(100, 100), new Velocity(-400, -300), Direction.None, new Acceleration(0, 0), 1, new Velocity(-160, -120), new Coordinates(-180, -110)],
+        [new Coordinates(100, 100), new Velocity(50, 50), Direction.Down | Direction.Right, new Acceleration(0, 0), 1, new Velocity(250, 250), new Coordinates(250, 250)],
+        [new Coordinates(100, 100), new Velocity(50, 50), Direction.Down | Direction.Right, new Acceleration(0, 0), 0.5, new Velocity(150, 150), new Coordinates(150, 150)],
+        [new Coordinates(100, 100), new Velocity(400, 300), Direction.None, new Acceleration(0, 0), 1, new Velocity(200, 100), new Coordinates(400, 300)],
+        [new Coordinates(100, 100), new Velocity(-400, -300), Direction.None, new Acceleration(0, 0), 1, new Velocity(-200, -100), new Coordinates(-200, -100)],
 
         // Custom acceleration
-        [new Coordinates(100, 100), new Velocity(50, 50), Direction.None, new Acceleration(40, 60), 1, new Velocity(90, 110), new Coordinates(170, 180)],
-        [new Coordinates(100, 100), new Velocity(50, 50), Direction.None, new Acceleration(-40, -60), 2, new Velocity(-30, -70), new Coordinates(120, 80)],
+        // [new Coordinates(100, 100), new Velocity(50, 50), Direction.None, new Acceleration(40, 60), 1, new Velocity(90, 110), new Coordinates(170, 180)],
+        // [new Coordinates(100, 100), new Velocity(50, 50), Direction.None, new Acceleration(-40, -60), 2, new Velocity(-30, -70), new Coordinates(120, 80)],
 
         // Combined direction and custom acceleration
-        [new Coordinates(100, 100), new Velocity(50, 50), Direction.Up, new Acceleration(80, 20), 1, new Velocity(130, -130), new Coordinates(190, 60)],
+        // [new Coordinates(100, 100), new Velocity(50, 50), Direction.Up, new Acceleration(80, 20), 1, new Velocity(130, -130), new Coordinates(190, 60)],
 
         // Acceleration limit
-        [new Coordinates(100, 100), new Velocity(50, 50), Direction.None, new Acceleration(400, 300), 1, new Velocity(290, 230), new Coordinates(270, 240)],
+        // [new Coordinates(100, 100), new Velocity(50, 50), Direction.None, new Acceleration(400, 300), 1, new Velocity(290, 230), new Coordinates(270, 240)],
 
         // Velocity limit
-        [new Coordinates(100, 100), new Velocity(360, 270), Direction.None, new Acceleration(80, 60), 1, new Velocity(400, 300), new Coordinates(480, 385)],
+        // [new Coordinates(100, 100), new Velocity(360, 270), Direction.None, new Acceleration(80, 60), 1, new Velocity(400, 300), new Coordinates(480, 385)],
 
         // Stopped
         [new Coordinates(100, 100), new Velocity(0, 0), Direction.None, new Acceleration(0, 0), 1, new Velocity(0, 0), new Coordinates(100, 100)],
         [new Coordinates(100, 100), new Velocity(0, 0), Direction.Up, new Acceleration(0, 0), 1, new Velocity(0, -200), new Coordinates(100, 0)],
-        [new Coordinates(100, 100), new Velocity(0, 0), Direction.None, new Acceleration(40, 60), 1, new Velocity(40, 60), new Coordinates(120, 130)]
+        // [new Coordinates(100, 100), new Velocity(0, 0), Direction.None, new Acceleration(40, 60), 1, new Velocity(40, 60), new Coordinates(120, 130)]
     ])('processFrame() startingPosition: %p, startingVelocity: %p, direction: %p, customAcceleration: %p, duration: %p, expectedVelocity: %p, expectedPosition: %p', 
             (startingPosition: Coordinates, startingVelocity: Velocity, direction: Direction, customAcceleration: Acceleration, duration: number, expectedVelocity: Velocity, expectedPosition: Coordinates) => {
         let subject = new Ship(startingPosition);
 
-        subject.velocity = startingVelocity;
+        // subject.velocity = startingVelocity;
+        subject.directionalVelocity = startingVelocity;
 
         subject.processFrame(direction, customAcceleration, duration);
         
-        expect(subject.velocity).toEqual(expectedVelocity);
+        // expect(subject.velocity).toEqual(expectedVelocity);
+        expect(subject.directionalVelocity).toEqual(expectedVelocity);
         expect(subject.position).toEqual(expectedPosition);
     });
 });
