51d2ea93933f111112db5ab22cebd1b0f7e58fe6
Maikel Bos <maikel.bos0@gmail.com>
2023-10-06 16:19:53 +0200

    Add boundary tests to processInput

--- tests/ship.test.ts
+++ tests/ship.test.ts
@@ -42,33 +42,40 @@ describe('Ship', () => {
 
     it.each([
         // Directions
-        [new Velocity(100, 0), Direction.Right, null, 1, new Velocity(1000, 0)],
-        [new Velocity(100, 0), Direction.Right, null, 0.1, new Velocity(300, 0)],
-        [new Velocity(0, 0), Direction.Right | Direction.Down, null, 0.1, new Velocity(141.4214, 141.4214)],
+        [new Coordinates(500, 500), new Velocity(100, 0), Direction.Right, null, 1, new Velocity(1000, 0)],
+        [new Coordinates(500, 500), new Velocity(100, 0), Direction.Right, null, 0.1, new Velocity(300, 0)],
+        [new Coordinates(500, 500), new Velocity(0, 0), Direction.Right | Direction.Down, null, 0.1, new Velocity(141.4214, 141.4214)],
 
         // Desired position
-        [new Velocity(100, 0), Direction.None, new Coordinates(1000, 100), 1, new Velocity(1000, 0)],
-        [new Velocity(100, 0), Direction.None, new Coordinates(1000, 100), 0.1, new Velocity(300, 0)],
-        [new Velocity(0, 0), Direction.None, new Coordinates(1000, 1000), 0.1, new Velocity(141.4214, 141.4214)],
+        [new Coordinates(500, 500), new Velocity(100, 0), Direction.None, new Coordinates(1000, 500), 1, new Velocity(1000, 0)],
+        [new Coordinates(500, 500), new Velocity(100, 0), Direction.None, new Coordinates(1000, 500), 0.1, new Velocity(300, 0)],
+        [new Coordinates(500, 500), new Velocity(0, 0), Direction.None, new Coordinates(1000, 1000), 0.1, new Velocity(141.4214, 141.4214)],
 
         // Combined
-        [new Velocity(100, 0), Direction.Right, new Coordinates(1000, 100), 0.1, new Velocity(300, 0)],
-        [new Velocity(0, 0), Direction.Left, new Coordinates(100, 1000), 0.1, new Velocity(-141.4214, 141.4214)],
+        [new Coordinates(500, 500), new Velocity(100, 0), Direction.Right, new Coordinates(1000, 500), 0.1, new Velocity(300, 0)],
+        [new Coordinates(500, 500), new Velocity(0, 0), Direction.Left, new Coordinates(500, 1000), 0.1, new Velocity(-141.4214, 141.4214)],
 
         // Velocity limit
-        [new Velocity(1000, 0), Direction.Right, null, 1, new Velocity(1000, 0)],
-        [new Velocity(500, 0), Direction.Right, null, 1, new Velocity(1000, 0)],
+        [new Coordinates(500, 500), new Velocity(1000, 0), Direction.Right, null, 1, new Velocity(1000, 0)],
+        [new Coordinates(500, 500), new Velocity(500, 0), Direction.Right, null, 1, new Velocity(1000, 0)],
 
         // Decelerating
-        [new Velocity(1000, 0), Direction.None, null, 1, new Velocity(0, 0)],
-        [new Velocity(1000, 0), Direction.None, null, 0.2, new Velocity(600, 0)],
-        [new Velocity(-150, -200), Direction.None, null, 0.1, new Velocity(-30, -40)],
+        [new Coordinates(500, 500), new Velocity(1000, 0), Direction.None, null, 1, new Velocity(0, 0)],
+        [new Coordinates(500, 500), new Velocity(1000, 0), Direction.None, null, 0.2, new Velocity(600, 0)],
+        [new Coordinates(500, 500), new Velocity(-150, -200), Direction.None, null, 0.1, new Velocity(-30, -40)],
 
         // Stopped
-        [new Velocity(0, 0), Direction.None, null, 1, new Velocity(0, 0)],
-    ])('processInput() startingVelocity: %p, direction: %p, desiredPosition: %p, duration: %p, expectedVelocity: %p', 
-            (startingVelocity: Velocity, direction: Direction, desiredPosition: Coordinates | null, duration: number, expectedVelocity: Velocity) => {
-        let subject = new Ship(new Coordinates(100, 100));
+        [new Coordinates(500, 500), new Velocity(0, 0), Direction.None, null, 1, new Velocity(0, 0)],
+
+        // Boundary checks
+        [new Coordinates(900, 500), new Velocity(1000, 0), Direction.Right, null, 0.1, new Velocity(400, 0)],
+        [new Coordinates(100, 500), new Velocity(-1000, 0), Direction.Left, null, 0.1, new Velocity(-400, 0)],
+        [new Coordinates(500, 900), new Velocity(0, 1000), Direction.Down, null, 0.1, new Velocity(0, 400)],
+        [new Coordinates(500, 100), new Velocity(0, -1000), Direction.Up, null, 0.1, new Velocity(0, -400)],
+        [new Coordinates(900, 900), new Velocity(1000, 1000), Direction.Right | Direction.Down, null, 0.1, new Velocity(400, 400)],
+    ])('processInput() startingPosition: %p, startingVelocity: %p, direction: %p, desiredPosition: %p, duration: %p, expectedVelocity: %p', 
+            (startingPosition: Coordinates, startingVelocity: Velocity, direction: Direction, desiredPosition: Coordinates | null, duration: number, expectedVelocity: Velocity) => {
+        let subject = new Ship(startingPosition);
 
         subject.velocity = startingVelocity;
 
