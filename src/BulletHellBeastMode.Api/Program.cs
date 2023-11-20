3a938a79c866f7349105325a914f206872aaa070
Maikel Bos <maikel.bos0@gmail.com>
2023-11-20 20:38:36 +0100

    Add basic http call from client to api

--- src/BulletHellBeastMode.Api/Program.cs
+++ src/BulletHellBeastMode.Api/Program.cs
@@ -1,9 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.WithOrigins("http://localhost:4200")));
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.MapGet("/test", () => "Hello World");
+app.UseCors();
+
+app.MapGet("/test", () => new { Text = "Hello World" });
 
 app.Run();
--- src/bullet-hell-beast-mode-client/src/app/app.component.ts
+++ src/bullet-hell-beast-mode-client/src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'bullet-hell-beast-mode';
+  response?: Response;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get<Response>("https://localhost:7263/test").subscribe(
+      (response) => this.response = response
+    );
+  }
+}
+
+class Response {
+  text?: string;
 }
