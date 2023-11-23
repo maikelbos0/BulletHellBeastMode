d4d8d8ad87a1a31464117d707e54237928a60e7f
Maikel Bos <maikel.bos0@gmail.com>
2023-11-23 20:29:31 +0100

    Add prettier and eslint plugins

--- src/bullet-hell-beast-mode-client/src/app/app.component.spec.ts
+++ src/bullet-hell-beast-mode-client/src/app/app.component.spec.ts
@@ -1,14 +1,14 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent]
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have the 'bullet-hell-beast-mode' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('bullet-hell-beast-mode');
+    expect(app.title).toEqual("bullet-hell-beast-mode");
   });
 
-  it('should render title', () => {
+  it("should render title", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, bullet-hell-beast-mode');
+    expect(compiled.querySelector("h1")?.textContent).toContain("Hello, bullet-hell-beast-mode");
   });
 });
--- src/bullet-hell-beast-mode-client/src/app/app.component.ts
+++ src/bullet-hell-beast-mode-client/src/app/app.component.ts
@@ -1,25 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [CommonModule, RouterOutlet, HttpClientModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss"
 })
 export class AppComponent implements OnInit {
-  title = 'bullet-hell-beast-mode';
+  title = "bullet-hell-beast-mode";
   response?: Response;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get<Response>("https://localhost:7263/test").subscribe(
-      (response) => this.response = response
-    );
+    this.http.get<Response>("https://localhost:7263/test").subscribe((response) => (this.response = response));
   }
 }
 
