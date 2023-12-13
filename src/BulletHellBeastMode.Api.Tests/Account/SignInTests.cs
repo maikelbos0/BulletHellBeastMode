3a7b678260c69d8edcf056f433c77c70e1d82d15
Maikel Bos <maikel.bos0@gmail.com>
2023-12-13 21:06:52 +0100

    Use test helpers for sign in tests

--- src/BulletHellBeastMode.Api.Tests/Account/SignInTests.cs
+++ src/BulletHellBeastMode.Api.Tests/Account/SignInTests.cs
@@ -1,13 +1,10 @@
 ﻿using BulletHellBeastMode.Api.Account;
-using System.Net.Http.Json;
+using BulletHellBeastMode.Api.Entities;
+using Microsoft.EntityFrameworkCore;
 using System.Net;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Xunit;
-using BulletHellBeastMode.Api.Entities;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Options;
-using System.Linq;
-using Microsoft.EntityFrameworkCore;
 
 namespace BulletHellBeastMode.Api.Tests.Account;
 
@@ -19,39 +16,31 @@ public class SignInTests(WebApplicationFactory factory) : IntegrationTestBase(fa
 
         var response = await Client.PostAsJsonAsync("/account/sign-in", new SignInUserCommand("sign-in-user", "password"));
 
-        // TODO extract some response stuff here?
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.True((await response.Content.ReadFromJsonAsync<CommandResult>())?.IsSuccess);
 
-        var content = await response.Content.ReadFromJsonAsync<CommandResult>();
-        Assert.NotNull(content);
-        Assert.True(content.IsSuccess);
-
-        using (var contextProvider = CreateContextProvider()) {
-            var updatedUser = contextProvider.Context.Users.Include(user => user.Events).Single(user => user.Name == "sign-in-user");
-            Assert.Equal(UserEventType.SignedIn, Assert.Single(updatedUser.Events).Type);
-        }
+        await ExecuteOnContext(async context => {
+            var user = await context.Users.Include(user => user.Events).SingleAsync(user => user.Name == "sign-in-user");
+            Assert.Equal(UserEventType.SignedIn, Assert.Single(user.Events).Type);
+        });
 
         Assert.Equal(HttpStatusCode.OK, (await Client.GetAsync("/account")).StatusCode);
     }
 
     [Fact]
     public async Task SignIn_With_Old_HashAlgorithmn_Rehashes_Password() {
-        var user = await CreateUser("rehash-user")
-            .WithPasswordHasherCompatibilityMode(PasswordHasherCompatibilityMode.IdentityV2);
+        var user = await CreateUser("rehash-user", true);
 
         var response = await Client.PostAsJsonAsync("/account/sign-in", new SignInUserCommand("rehash-user", "password"));
 
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.True((await response.Content.ReadFromJsonAsync<CommandResult>())?.IsSuccess);
 
-        var content = await response.Content.ReadFromJsonAsync<CommandResult>();
-        Assert.NotNull(content);
-        Assert.True(content.IsSuccess);
-
-        using (var contextProvider = CreateContextProvider()) {
-            var updatedUser = contextProvider.Context.Users.Include(user => user.Events).Single(user => user.Name == "rehash-user");
+        await ExecuteOnContext(async context => {
+            var updatedUser = await context.Users.Include(user => user.Events).SingleAsync(user => user.Name == "rehash-user");
             Assert.NotEqual(updatedUser.Password, user.Password);
             Assert.Equal(UserEventType.SignedIn, Assert.Single(updatedUser.Events).Type);
-        }
+        });
 
         Assert.Equal(HttpStatusCode.OK, (await Client.GetAsync("/account")).StatusCode);
     }
@@ -61,10 +50,7 @@ public class SignInTests(WebApplicationFactory factory) : IntegrationTestBase(fa
         var response = await Client.PostAsJsonAsync("/account/sign-in", new SignInUserCommand("not-a-user", "password"));
 
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-
-        var content = await response.Content.ReadFromJsonAsync<CommandResult>();
-        Assert.NotNull(content);
-        Assert.False(content.IsSuccess);
+        Assert.False((await response.Content.ReadFromJsonAsync<CommandResult>())?.IsSuccess);
 
         Assert.Equal(HttpStatusCode.Unauthorized, (await Client.GetAsync("/account")).StatusCode);
     }
@@ -76,15 +62,12 @@ public class SignInTests(WebApplicationFactory factory) : IntegrationTestBase(fa
         var response = await Client.PostAsJsonAsync("/account/sign-in", new SignInUserCommand("password-user", "wrong"));
 
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.False((await response.Content.ReadFromJsonAsync<CommandResult>())?.IsSuccess);
 
-        var content = await response.Content.ReadFromJsonAsync<CommandResult>();
-        Assert.NotNull(content);
-        Assert.False(content.IsSuccess);
-
-        using (var contextProvider = CreateContextProvider()) {
-            var updatedUser = contextProvider.Context.Users.Include(user => user.Events).Single(user => user.Name == "password-user");
+        await ExecuteOnContext(async context => {
+            var updatedUser = await context.Users.Include(user => user.Events).SingleAsync(user => user.Name == "password-user");
             Assert.Equal(UserEventType.FailedSignIn, Assert.Single(updatedUser.Events).Type);
-        }
+        });
 
         Assert.Equal(HttpStatusCode.Unauthorized, (await Client.GetAsync("/account")).StatusCode);
     }
