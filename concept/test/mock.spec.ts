ed9762b0fcf18fd8abf3790bf68d2d760e08e996
Maikel Bos <maikel.bos0@gmail.com>
2024-01-21 11:43:40 +0100

    Add mock object

---
+++ concept/test/mock.spec.ts
@@ -0,0 +1,59 @@
+import { Mock } from './mock';
+import { Any } from './any';
+
+describe('Mock', () => {
+    test('constructor() creates properties', () => {
+        const subject = new Mock({
+            value: 10
+        });
+
+        expect(subject.object).toBeTruthy();
+        expect(subject.object.value).toBe(10);
+    });
+
+    test('constructor() creates methods', () => {
+        const subject = new Mock({
+            multiply(x: number, y: number): number{
+                return x * y;
+            }
+        });
+
+        expect(subject.object).toBeTruthy();
+        expect(typeof subject.object.multiply).toBe('function');
+        expect(subject.object.multiply(3, 4)).toBe(12);
+    });
+
+    it.each([
+        [Any.value, Any.value],
+        [3, 4],
+    ])('received() x: %p, y: %p', (x: any, y: any) => {
+        const subject = new Mock({
+            multiply(x: number, y: number): number {
+                return x * y;
+            }
+        });
+
+        subject.object.multiply(3, 4);
+
+        subject.received('multiply', x, y);
+    });
+
+    it.each([
+        [Any.value, 5],
+        [3, 5],
+    ])('received() requires method to have been called x: %p, y: %p', (x: any, y: any) => {
+        const subject = new Mock({
+            multiply(x: number, y: number): number {
+                return x * y;
+            }
+        });
+
+        subject.object.multiply(2, 3);
+        
+        const action = () => {
+            subject.received('multiply', x, y);
+        };
+
+        expect(action).toThrow();
+    });
+});
---
+++ concept/test/mock.ts
@@ -0,0 +1,53 @@
+import { Any } from "./any";
+
+export class Mock {
+    methodCalls = new Map<string, any[][]>();
+    object: any = {
+        mock: this
+    };
+
+    constructor(template: any) {
+        for (let key in template) {
+            if (typeof template[key] === 'function') {
+                this.object[key] = (...args: any[]) => {
+                    const receivedArgs = this.methodCalls.get(key);
+
+                    if (receivedArgs) {
+                        receivedArgs.push(args);
+                    }
+                    else {
+                        this.methodCalls.set(key, [args]);
+                    }
+
+                    return template[key](...args);
+                }
+            }
+            else {
+                this.object[key] = template[key];
+            }
+        }
+    }
+
+    received(methodName: string, ...args: any[]) {
+        const allReceivedArgs = this.methodCalls.get(methodName);
+        let pass = false;
+
+        allReceivedArgs?.forEach((receivedArgs) => {
+            if (receivedArgs.length !== args.length) {
+                return;
+            }
+
+            for (let i = 0; i < args.length; i++) {
+                if (receivedArgs[i] !== args[i] && !(typeof args[i]['matches'] === 'function' && (args[i] as Any).matches(receivedArgs[i]))) {
+                    return;
+                }
+            }
+
+            pass = true;
+        });
+
+        if (!pass) {
+            throw new Error(`No matching calls found: ${methodName}(${args.join(', ')});`);
+        }
+    }
+}
