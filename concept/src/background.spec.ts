73f2e6bf98d00039d5bd5ee9f82f02bdfd4523fd
Maikel Bos <maikel.bos0@gmail.com>
2024-01-21 12:21:25 +0100

    Add render test

--- concept/src/background.spec.ts
+++ concept/src/background.spec.ts
@@ -1,4 +1,8 @@
+import { Any } from '../test/any';
+import { Mock } from '../test/mock';
 import { Background } from './background';
+import { Game } from './game';
+import { RenderingOptions } from './rendering-options';
 
 describe('Background', () => {
     test('constructor()', () => {
@@ -22,5 +26,32 @@ describe('Background', () => {
         expect(subject.offset).toEqual(expectedOffset);
     })
 
-    // TODO expand tests for rendering
+    test('render()', () => {
+        const subject = new Background();
+        subject.offset = 180;
+
+        const renderingContextMock = new Mock({
+            rectangle(x0: number, y0: number, x1: number, y1: number, options: RenderingOptions): void { }
+        });
+
+        subject.render(renderingContextMock.object);
+
+        renderingContextMock.received('rectangle', 0, subject.offset, Game.width, subject.lineHeight, Any.matching({
+            fill: Any.matching({
+                x0: 0,
+                y0: subject.offset,
+                x1: 0,
+                y1: subject.offset + subject.lineHeight
+            })
+        }));
+
+        renderingContextMock.received('rectangle', 0, subject.offset + subject.lineHeight, Game.width, subject.lineHeight, Any.matching({
+            fill: Any.matching({
+                x0: 0,
+                y0: subject.offset + subject.lineHeight,
+                x1: 0,
+                y1: subject.offset + 2 * subject.lineHeight
+            })
+        }));
+    });
 });
