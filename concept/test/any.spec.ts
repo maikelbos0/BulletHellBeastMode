33cfe00fe8723241bf235516547167f0fabb1d35
Maikel Bos <maikel.bos0@gmail.com>
2024-01-21 11:40:10 +0100

    Add Any object for mock call verification

---
+++ concept/test/any.spec.ts
@@ -0,0 +1,32 @@
+import { Any } from './any';
+
+describe('Any', () => {
+    it.each([
+        [undefined],
+        [null],
+        ['Foo'],
+        [42],
+    ])('value value: %p', (value: any) => {
+        expect(Any.value.matches(value)).toBeTruthy();
+    });
+
+    it.each([
+        [() => { }, true],
+        [(value: any) => value, true],
+        [undefined, false],
+        [null, false],
+        [42, false]
+    ])('function value: %p, expectedResult: %p', (value: any, expectedResult: boolean) => {
+        expect(Any.function.matches(value)).toBe(expectedResult);
+    });
+
+    it.each([
+        [{ foo: 1 }, { foo: 1 }, true],
+        [{ foo: 1 }, { foo: 2 }, false],
+        [{ foo: 1 }, { bar: 1 }, false],
+        [{ foo: 1, bar: 'yes' }, { foo: 1 }, true],
+        [{ foo: 1 }, { foo: Any.value }, true],
+    ])('matching() argument: %p, value %p, expectedResult: %p', (value: any, argument: any, expectedResult: boolean) => {
+        expect(Any.matching(argument).matches(value)).toBe(expectedResult);
+    });
+});
---
+++ concept/test/any.ts
@@ -0,0 +1,19 @@
+export class Any {
+    static readonly value: Any = new Any((_value: any) => true);
+
+    static readonly function: Any = new Any((value: any) => typeof value === 'function');
+
+    static matching(argument: any) {
+        return new Any((value: any) => {
+            for (let key in argument) {
+                if (value[key] !== argument[key] && !(typeof argument[key]['matches'] === 'function' && (argument[key] as Any).matches(value[key]))) {
+                    return false;
+                }
+            }
+
+            return true;
+        });
+    }
+
+    constructor(readonly matches: (value: any) => boolean) { }
+}
