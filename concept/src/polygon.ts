35aa9084b6f1d2a6bf31156deb9722fb1e04182a
Maikel Bos <maikel.bos0@gmail.com>
2024-01-22 21:01:14 +0100

    Add first version of full translate/scale/rotate transform when dead

--- concept/src/polygon.ts
+++ concept/src/polygon.ts
@@ -1,10 +1,14 @@
 import { Coordinates } from './coordinates.js';
 import { LineSegment } from './line-segment.js';
 import { RenderingContext } from './rendering-context.js';
+import { Transformation } from "./transformation.js";
 
 export class Polygon {
+    static readonly maximumRotationWhenDead: number = Math.PI * 2;
+
     readonly coordinates: Coordinates[];
     readonly centerPoint: Coordinates;
+    readonly rotationWhenDead: number;
 
     constructor(...coordinates: Coordinates[]) {
         const coordinateCount = coordinates.length;
@@ -43,30 +47,43 @@ export class Polygon {
 
         this.coordinates = coordinates;
         this.centerPoint = coordinates.reduce((c, coordinates) => c.add(coordinates), new Coordinates(0, 0)).divide(coordinateCount);
+        this.rotationWhenDead = Math.random() * Polygon.maximumRotationWhenDead * 2 - Polygon.maximumRotationWhenDead;
     }
 
+    // TODO perhaps refactor - map coordinates around center point and only add transformations in if, it saves a bunch of math in the render loop
+    // TODO bring under test
     render(context: RenderingContext, deadForDuration: number | undefined): void {
+        if (deadForDuration === undefined) {
+            context.moveTo(this.coordinates[this.coordinates.length - 1].x, this.coordinates[this.coordinates.length - 1].y);
+    
+            this.coordinates.forEach(coordinates => {
+                context.lineTo(coordinates.x, coordinates.y);
+            });
+        }
+        else {
+            // TODO make drift variable
+            const drift = 1 + Math.sqrt(deadForDuration) * 5;
 
-        const startingCoordinates = this.transformCoordinates(this.coordinates[this.coordinates.length - 1], deadForDuration);
-        context.moveTo(startingCoordinates.x, startingCoordinates.y);
-
-        this.coordinates.forEach(coordinates => {
-            const transformedCoordinates = this.transformCoordinates(coordinates, deadForDuration);
-            context.lineTo(transformedCoordinates.x, transformedCoordinates.y);
-        });
+            context.transform(() => {
+                const startingCoordinates = this.transformCoordinates(this.coordinates[this.coordinates.length - 1]);
+                context.moveTo(startingCoordinates.x, startingCoordinates.y);
+        
+                this.coordinates.forEach(coordinates => {
+                    const transformedCoordinates = this.transformCoordinates(coordinates);
+                    context.lineTo(transformedCoordinates.x, transformedCoordinates.y);
+                });
+                },
+                Transformation.translate(drift * this.centerPoint.x, drift * this.centerPoint.y),
+                Transformation.rotate(deadForDuration * this.rotationWhenDead),
+                Transformation.scale(1 / (1 + deadForDuration))
+            );
+        }
     }
 
-    transformCoordinates(coordinates: Coordinates, deadForDuration: number | undefined): Coordinates {
-        if (deadForDuration === undefined) {
-            return coordinates;
-        }
-
-        const scale = 1 / (1 + deadForDuration);
-        const driftSpeed = Math.sqrt(deadForDuration) * 5;
-
+    transformCoordinates(coordinates: Coordinates): Coordinates {
         return new Coordinates(
-            scale * coordinates.x + driftSpeed * this.centerPoint.x,
-            scale * coordinates.y + driftSpeed * this.centerPoint.y
+            coordinates.x - this.centerPoint.x,
+            coordinates.y - this.centerPoint.y
         );
     }
 }
